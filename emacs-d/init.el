8ee5060c4c01d873ecf3924b23b81d2fb32f0ab9
Sandeep Jagtap <sandeep.jagtap@gmail.com>
2011-08-06 16:30:32 -0500

    js2-mode config

--- emacs-d/init.el
+++ emacs-d/init.el
@@ -76,4 +76,8 @@
 (if (file-exists-p user-specific-dir)
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+
 ;;; init.el ends here
